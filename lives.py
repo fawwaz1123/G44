4b9751722cdfa93116b09f9b92680d1a7fb6b462
fawwaz1123 <fawwazhahmed@gmail.com>
2025-04-03 00:33:55 +0100

    cleaned up

--- lives.py
+++ lives.py
@@ -1,22 +1,24 @@
-try:
-    import simplegui
-except ImportError:
-    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-
-from variables import HEART_IMG
 class Lives:
     def __init__(self):
         self.one_life = True
         self.two_lives = True
         self.three_lives = True
 
-    def draw(self, canvas):
+    def draw(self, canvas, heart_img):
         if self.one_life:
-            canvas.draw_image(HEART_IMG, (60, 60), (120, 120), (800, 25), (60, 60))
+            canvas.draw_image(heart_img, (60, 60), (120, 120), (800, 25), (60, 60))
         if self.two_lives:
-            canvas.draw_image(HEART_IMG, (60, 60), (120, 120), (860, 25), (60, 60))
+            canvas.draw_image(heart_img, (60, 60), (120, 120), (860, 25), (60, 60))
         if self.three_lives:
-            canvas.draw_image(HEART_IMG, (60, 60), (120, 120), (920, 25), (60, 60))
+            canvas.draw_image(heart_img, (60, 60), (120, 120), (920, 25), (60, 60))
+        if not self.one_life:
+            canvas.draw_image(heart_img, (60, 180), (120, 120), (800, 25), (60, 60))
+        if not self.two_lives:
+            canvas.draw_image(heart_img, (60, 180), (120, 120), (860, 25), (60, 60))
+        if not self.three_lives:
+            canvas.draw_image(heart_img, (60, 180), (120, 120), (920, 25), (60, 60))
 
-    # The empty heart drawing from loose_life() has been merged into draw()
-  # (i.e. both full and missing lives are drawn within draw())
+    def reset(self):
+        self.one_life = True
+        self.two_lives = True
+        self.three_lives = True
