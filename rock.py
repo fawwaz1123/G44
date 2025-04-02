91319b296e41c1598863d7035f9af292062132e7
fawwaz1123 <fawwazhahmed@gmail.com>
2025-04-03 00:40:00 +0100

    spawns rock image

--- rock.py
+++ rock.py
@@ -1,33 +1,31 @@
-from vector import Vector 
 try:
-    import simplegui
-except ImportError:
-    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui  
-from variables import WIDTH, HEIGHT
-import random
-
+    from user305_o32FtUyCKk_0 import Vector
+except:
+    from vector import Vector
 class Rock:
-    def __init__(self, truck, spawn_x):
-        self.t = truck  
+    def __init__(self, truck, spawn_x, height):
+        self.t = truck
         self.x = spawn_x
-        self.y = HEIGHT - 100
+        self.y = height - 100
         self.pos = Vector(self.x, self.y)
         self.collide = False
+        self.already_hit = False
+        self.hit_frame = 0
 
-    def draw(self, canvas, offset):
-        canvas.draw_circle((self.pos.x - offset, self.pos.y), 50, 1, 'Grey', 'Grey')
-
+    def draw(self, canvas, offset, rock_img):
+        canvas.draw_circle((self.pos.x - offset, self.pos.y), 20, 1, 'Grey', 'Grey')
+        canvas.draw_image(
+            rock_img, 
+            (125, 91.5), 
+            (250, 183), 
+            (self.pos.x - offset, self.pos.y - 25), 
+            (125, 91.5)
+        )
+    
     def collided(self):
-        # Get the positions of both wheels.
         front_wheel = self.t.front_wheel.pos
         back_wheel = self.t.back_wheel.pos
-        # Compute the average (midpoint) of the two wheel positions.
         avg_wheel = (front_wheel + back_wheel) * 0.5
-        # Determine the distance between the rock's center and the average wheel position.
         d = avg_wheel.copy().subtract(self.pos).length()
-        # Use a threshold (here, 50 pixels) to decide if a collision occurred.
-        if d <= 50:
-            self.collide = True
-        else:
-            self.collide = False
+        self.collide = (d <= 50 and not self.already_hit)
         return self.collide
